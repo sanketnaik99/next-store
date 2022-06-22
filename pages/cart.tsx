89d473be1d736548118fe33b24e071e8cf8002c4
Sanket Naik <sanketnaik99@gmail.com>
2022-06-21 23:15:31 -0500

    Cart items list added

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -7,10 +7,14 @@ import {
   CardMedia,
   Divider,
   Grid,
+  Paper,
+  Stack,
   Typography,
+  useTheme,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { NextPage } from "next";
+import Image from "next/image";
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../ducks";
@@ -19,6 +23,7 @@ const Cart: NextPage = () => {
   const cartItems = useSelector<RootState, LineItem[]>(
     (state) => state.cart.cart.line_items
   );
+  const theme = useTheme();
 
   return (
     <>
@@ -31,43 +36,83 @@ const Cart: NextPage = () => {
         </Typography>
       </Grid>
       <Grid container sx={{ padding: 3 }}>
-        <Grid item container spacing={2} xs={12} md={6}>
-          {cartItems.map((item) => (
-            <Grid item xs={12} md={6} key={item.id}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  alt="green iguana"
-                  height="200"
-                  image={item.image!.url}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {item.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {item.quantity}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
+        <Grid item xs={12} md={8}>
+          <Stack spacing={2}>
+            {cartItems.map((item) => (
+              <Paper key={item.id}>
+                <Grid container justifyContent="center" sx={{ padding: 1 }}>
+                  <Grid item xs={4}>
+                    <Image
+                      src={item!.image!.url}
+                      alt={item.name}
+                      width={400}
+                      height={200}
+                      layout="responsive"
+                    />
+                  </Grid>
+                  <Grid item xs={6} sx={{ paddingLeft: "10px" }}>
+                    <Typography variant="h6" component="h6" fontWeight={600}>
+                      {item.name}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      component="p"
+                      sx={{
+                        color:
+                          theme.palette.mode === "dark" ? "#BDBDBD" : "#757575",
+                      }}
+                    >
+                      {item.sku}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography
+                      variant="h6"
+                      component="h6"
+                      textAlign="center"
+                      fontWeight={600}
+                    >
+                      {item.price.formatted_with_symbol}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      component="p"
+                      textAlign="center"
+                      sx={{
+                        color:
+                          theme.palette.mode === "dark" ? "#BDBDBD" : "#757575",
+                      }}
+                    >
+                      Qty: {item.quantity}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Paper>
+            ))}
+          </Stack>
         </Grid>
-        <Grid item xs={12} md={1}>
-          <Divider
-            orientation="vertical"
-            sx={{ display: { xs: "none", md: "flex" } }}
-          />
-          <Divider
-            orientation="horizontal"
-            sx={{ display: { xs: "flex", md: "none" } }}
-          />
+        <Grid
+          item
+          xs={12}
+          md={1}
+          sx={{
+            padding: { xs: 2, md: 0 },
+            display: { xs: "block", md: "none" },
+          }}
+        >
+          <Divider />
         </Grid>
-        <Grid item xs={12} md={5}>
+        <Grid
+          item
+          xs={12}
+          md={1}
+          container
+          justifyContent="center"
+          sx={{ display: { xs: "none", md: "flex" } }}
+        >
+          <Divider orientation="vertical" variant="middle" />
+        </Grid>
+        <Grid item xs={12} md={3}>
           <Box height={500}></Box>
         </Grid>
       </Grid>
