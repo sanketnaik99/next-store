4eb8be1c93478be03fc3bd18bf139b188568e8a5
Sanket Naik <sanketnaik99@gmail.com>
2022-07-17 22:29:32 -0500

    basic login logic implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,18 @@ import Layout from "../components/Layout/Layout/Layout";
 import Commerce from "@chec/commerce.js";
 import { Provider } from "react-redux";
 import { store } from "../ducks";
+import { useEffect } from "react";
 
 export const commerce = new Commerce(
   `${process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY}`
 );
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    console.log("IS LOGGED IN", commerce.customer.isLoggedIn());
+    console.log("About", commerce.customer.about());
+  }, []);
+
   return (
     <Provider store={store}>
       <Layout>
--- pages/sign-in/callback/[token].tsx
+++ pages/sign-in/callback/[token].tsx
@@ -1,9 +1,26 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
+import { commerce } from "../../_app";
 
 const SignInWithToken = () => {
   const router = useRouter();
   const query = router.query;
+
+  const handleUserSignIn = async (token: string) => {
+    commerce.customer
+      .getToken(token)
+      .then((jwt) => {
+        console.log(jwt);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    if (router.query.token) {
+      handleUserSignIn(router.query.token.toString());
+    }
+  }, [router.query]);
+
   console.log(query);
   return (
     <>
